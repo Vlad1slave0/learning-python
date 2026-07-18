2a1fe20e42ab73dad067e3c06745010be9065071
Vlad1slave0 <prololkin@gmail.com>
2026-07-18 18:57:40 +0200

    little changes

--- Day2/PracticeExercices/IndoorVoice.py
+++ Day2/PracticeExercices/IndoorVoice.py
@@ -1,14 +1,7 @@
 import time
 
-RED = "\033[31m"
 GREEN = "\033[32m"
 RESET = "\033[0m"
-
-for letter in inputPerson:
-    if letter.isupper():
-       RED + letter + RESET
-    else:
-        GREEN + letter + RESET
 
 inputPerson = input(f"Напишите что-то: ")
 LowLetters = inputPerson.lower()
